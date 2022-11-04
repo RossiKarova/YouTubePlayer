02575fa089110ead6531f47edc279daa8fe8c5e7
Rossi <karovarossi@gmail.com>
2022-11-04 10:10:54 +0000

    128 all working

--- app/src/main/java/com/example/youtubeplayer/StandaloneActivity.kt
+++ app/src/main/java/com/example/youtubeplayer/StandaloneActivity.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.youtube.player.YouTubeStandalonePlayer
-
+import kotlinx.android.synthetic.main.activity_standalone.*
 
 class StandaloneActivity : AppCompatActivity(), View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_standalone)
@@ -18,7 +19,21 @@ class StandaloneActivity : AppCompatActivity(), View.OnClickListener {
         btnPlayVideo.setOnClickListener(this)
         btnPlayList.setOnClickListener(this)
 
+//        btnPlayVideo.setOnClickListener(object : View.OnClickListener{
+//            override fun onClick(v: View?) {
+//                TODO("Not yet implemented")
+//            }
+//        })
 
+//        btPlayVideo.setOnClockListener(View.OnClickListener {v->
+//            TODO("Not yet implemented")
+//        })
+
+//        val listener = View.OnClickListener { v ->
+//            TODO("Not yet implemented")
+//        }
+//        btnPlayVideo.setOnClickListener(listener)
+//        btnPlayList.setOnClickListener(listener)
     }
 
     override fun onClick(view: View) {
